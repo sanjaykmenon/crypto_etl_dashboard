910f27343df16994f815bebfa7c63caf084d584c
Sanjay Krishna <3213319+sanjaykmenon@users.noreply.github.com>
2023-02-02 20:34:45 -0500

    update utc
    updated utc

--- test/unit/test_exchange_data_etl_unit.py
+++ test/unit/test_exchange_data_etl_unit.py
@@ -3,7 +3,7 @@ import datetime
 from bitcoinmonitor.exchange_data_etl import get_utc_from_unix_time
 
 
-# test to check if unix milliseconds are being converted to right time format.
+# test to check if unix milliseconds are being converted to right time format
 def test_get_utc_from_unix_time():
     ut: int = 1625249025588
     expected_dt = datetime.datetime(2021, 7, 2, 18, 3, 45, 588000)
